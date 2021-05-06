cf49bbceba31963d0bf20ba19c85c01d5a17e6be
humbleguidant <37128838+humbleguidant@users.noreply.github.com>
2021-05-06 00:29:27 -0800

    Update forgotPassword.js

--- AppPrototype/www/js/forgotPassword.js
+++ AppPrototype/www/js/forgotPassword.js
@@ -1,7 +1,26 @@
+/* 
+Author: Aubrey Nickerson
+Date: September 21st, 2020
+Program: forgotPassword.js
+Project: Construction App
+
+This is the forgot password script.
+It handles the user input and makes 
+an AJAX call to a remote server hosting a php
+script. The email gets sent to the remote server
+and the php script handles the email. The php script
+sends the data back to forgotPassword.js.
+*/
+
 $(document).ready(function() {
     $("#submit").click(function () {
-        var email = $("#empid").val();
+        
+        // assign email to variable.
+        var email = $("#empid").val();       
+        // Create parameters for HTTP request
         var dataString = "empid=" + email + "&submit=";
+        
+        // Make the AJAX call to remote server 
         $.ajax({
             type: "GET",
             url: "http://10.1.144.91/PHPCodeForBackend/forgotPassword.php",
@@ -11,6 +30,7 @@ $(document).ready(function() {
             beforeSend: function () {
                 $("#submit").val('Loading...');
             },
+            // If the AJAX call was successful then handle the repsonse. 
             success: function(data){
                 if(data === "This email is not registered in the system. Please sign up or contact the administrator."){
                     alert(data);
@@ -19,6 +39,7 @@ $(document).ready(function() {
                     alert(data);
                     return false;
                 }
+                // If no issues then redirect to the login page.
                 alert(data);
                 window.location = "../index.html";
             }
