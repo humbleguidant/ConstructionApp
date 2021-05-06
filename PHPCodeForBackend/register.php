c16c6fc15fda5d17549daff8b02757d7c108b4bb
humbleguidant <37128838+humbleguidant@users.noreply.github.com>
2021-05-06 01:43:00 -0800

    Update register.php

--- PHPCodeForBackend/register.php
+++ PHPCodeForBackend/register.php
@@ -6,15 +6,19 @@ Program: register.php
 Project: Construction App
 
 This php script receives an AJAX call from the 
-login javascript file. It receives the data
+register javascript file. It receives the data
 and creates a connection to the database. The data from
 the AJAX call will be handled with the database. 
-The database checks if the user exists.
+The database will create a new user.
 */
 
+
+// Create a MySQL object to make a connection and perform querys
     include("Mysql.php");
     $my_sql = new Mysql();
     $my_sqli = $my_sql->dbConnect();
+
+// Create a random password generator
     function randomPassword($length){
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!-.[]?*()';
         $password = '';
@@ -24,12 +28,18 @@ The database checks if the user exists.
         }
         return $password;
     }
+
+// IF the button has been clicked then proceed.
     if(isset($_POST['submit'])){
         $email = $_POST['empid'];
         $employee = $_POST['employee'];
         $admin = $_POST['admin'];
         $acceptConditions = $_POST['check'];
+        
+        // Create the random password
         $eightCharPassword = randomPassword(8);
+        
+        // Prepare the email and send new password to user
         $from = "<donotreply@zueblin.com>";
         $to = $email;
         $subject = "Your Password";
@@ -50,6 +60,8 @@ The database checks if the user exists.
         $headers .= "Content-type:text/html;charset=UTF-8"."\r\n";
         $headers .= "From: DoNotReply".$from."\r\n".
                     "X-Mailer: PHP/".phpversion();
+        
+        // If the user is an admin then insert the user as an admin in the database.
         if(isset($admin)){
             $sqlQuery = $my_sql->insertQuery($email, $eightCharPassword, $admin, $acceptConditions);
             if($sqlQuery){
@@ -62,6 +74,7 @@ The database checks if the user exists.
             $my_sql->dbDisconnect();
             exit(1);
         }
+        // If the user is an employee then insert the user as an employee
         $sqlQuery = $my_sql->insertQuery($email, $eightCharPassword, $employee, $acceptConditions);
         if($sqlQuery){
             mail($to, $subject, $message, $headers);
@@ -69,6 +82,8 @@ The database checks if the user exists.
             $my_sql->dbDisconnect();
             exit(1);
         }
+        
+        // If the email already exists then alert the user.
         echo "This email is already registered. Please login or contact administrator.";
         $my_sql->dbDisconnect();
     }
